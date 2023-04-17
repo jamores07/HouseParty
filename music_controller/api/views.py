7d39c32d344279c79bee5ece194e9cd5f7eb1b66
John Amores <kayak.stated_0b@icloud.com>
2023-04-17 14:17:52 -0400

    Session Functionality expanded

--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -4,6 +4,7 @@ from .serializer import RoomSerializer, CreateRoomSerializer
 from .models import Room
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.http import JsonResponse
 
 
 # Create your views here.
@@ -82,3 +83,15 @@ class JoinRoomView(APIView):
             return Response({'Bad Request': 'Invalid Room Code'}, status=status.HTTP_400_BAD_REQUEST)
 
         return Response({'Bad Request': 'Invalid post data, did not find a code key'}, status=status.HTTP_400_BAD_REQUEST)
+    
+
+class UserInRoomView(APIView):
+    def get(self, request, format=None):
+        if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create()
+
+        data = {
+            'code': self.request.session.get('room_code')
+        }    
+
+        return JsonResponse(data, status=status.HTTP_200_OK)
