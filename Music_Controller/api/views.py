f2e69534673346b3c1705966c68c8d87324d32f9
John Amores <kayak.stated_0b@icloud.com>
2023-04-23 18:39:27 -0400

    Added more Home Page Functionality

--- Music_Controller/api/views.py
+++ Music_Controller/api/views.py
@@ -4,6 +4,7 @@ from .serializers import RoomSerializer, CreateRoomSerializer
 from .models import Room
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.http import JsonResponse
 
 # Create your views here.
 
@@ -33,7 +34,19 @@ class JoinRoomView(APIView):
         return Response({'Bad Request': 'Invalid post data, did not find a code key'}, status=status.HTTP_400_BAD_REQUEST)
 
 
+class UserInRoom(APIView):
+    def get(self, request, format=None):
+        if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create()
+
+        data = {
+            'code': self.request.session.get('room_code')
+        }
+        return JsonResponse(data, status=status.HTTP_200_OK)
+
+
 ######## ROOM CRUD FUNCTIONS ########
+
 
 class CreateRoomView(APIView):
     serializer_class = CreateRoomSerializer
--- Music_Controller/frontend/src/components/Room.js
+++ Music_Controller/frontend/src/components/Room.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Grid, Button, Typography } from "@material-ui/core";
 
 export default class Room extends Component {
   constructor(props) {
@@ -26,12 +27,37 @@ export default class Room extends Component {
 
   render() {
     return (
-      <div>
-        <h3>{this.roomCode}</h3>
-        <p>Votes: {this.state.votesToSkip}</p>
-        <p>Guest Can Pause: {this.state.guestCanPause.toString()}</p>
-        <p>Host: {this.state.isHost.toString()}</p>
-      </div>
+      <Grid container spacing={1}>
+        <Grid item xs={12} align="center">
+          <Typography variant="h4" component="h4">
+            Code: {this.roomCode}
+          </Typography>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <Typography variant="h6" component="h6">
+            Votes: {this.state.votesToSkip}
+          </Typography>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <Typography variant="h6" component="h6">
+            Guest Can Pause: {this.state.guestCanPause.toString()}
+          </Typography>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <Typography variant="h6" component="h6">
+            Host: {this.state.isHost.toString()}
+          </Typography>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <Button
+            variant="contained"
+            color="secondary"
+            onClick={this.leaveButtonPressed}
+          >
+            Leave Room
+          </Button>
+        </Grid>
+      </Grid>
     );
   }
 }
