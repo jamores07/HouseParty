99601010b079ec65fefba50048ecd25ddc0c830c
John Amores <kayak.stated_0b@icloud.com>
2023-04-23 12:36:22 -0400

    Added Join Room Functionality

--- Music_Controller/api/urls.py
+++ Music_Controller/api/urls.py
@@ -3,5 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('room', RoomView.as_view()),
-    path('create-room', CreateRoomView.as_view())
+    path('create-room', CreateRoomView.as_view()),
+    path('get-room', GetRoomView.as_view()),
+    path('join-room', JoinRoomView.as_view())
 ]
