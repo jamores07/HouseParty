f096d26cd0e685702d1eafc1da179eef059781a4
John Amores <kayak.stated_0b@icloud.com>
2023-04-22 23:34:09 -0400

    POST Request Started

--- Music_Controller/api/urls.py
+++ Music_Controller/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import RoomView
+from .views import *
 
 urlpatterns = [
     path('room', RoomView.as_view()),
+    path('create-room', CreateRoomView.as_view())
 ]
